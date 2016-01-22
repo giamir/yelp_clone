5b7cff336f38fb847d2bd1b230f3b5d9cbaa16a9
giamir <giamir.buoncristiani@gmail.com>
2016-01-22 23:50:36 +0000

    fix minor bugs

--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -1,13 +1,16 @@
-$(document).ready(function() {
-
+var ready;
+ready = function() {
   $('.endorsements-link').on('click', function(event){
-      event.preventDefault();
-      event.stopImmediatePropagation();
+    event.preventDefault();
+    event.stopPropagation();
 
-      var endorsementCount = $(this).siblings('.endorsements_count');
+    var endorsementCount = $(this).siblings('.endorsements_count');
 
-      $.post(this.href, function(response){
-        endorsementCount.text(response.new_endorsement_count);
+    $.post(this.href, function(response){
+      endorsementCount.text(response.new_endorsement_count);
     });
   });
-});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
